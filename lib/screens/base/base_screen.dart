7647210dfa5f61c8b3510f086121b07ead2a4763
Jonatha Rihan <accenture.jrihan@bv.com.br>
2022-06-19 16:52:58 -0300

    :sparkles: navigation

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,8 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
 import 'package:xlo_mobx/screens/home/home_screen.dart';
+import 'package:xlo_mobx/stores/page_store.dart';
 
-class BaseScreen extends StatelessWidget {
+class BaseScreen extends StatefulWidget {
+  @override
+  State<BaseScreen> createState() => _BaseScreenState();
+}
+
+class _BaseScreenState extends State<BaseScreen> {
   final PageController pageController = PageController();
+
+  final PageStore pageStore = GetIt.I<PageStore>();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    reaction((_) => pageStore.page,
+        (page) => pageController.jumpToPage(page as int));
+  }
 
   @override
   Widget build(BuildContext context) {
